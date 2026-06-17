dca55905a552d31d793db64b9050d5b1b6895c5c
Chance <chance5234972@gmail.com>
2026-06-17 18:26:49 +0800

    Refactor layout structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -260,31 +260,31 @@ function MainApp() {
           </div>
         </div>
 
-        <div className="flex-1 w-full relative">
+        <div className="flex-1 w-full relative flex flex-col">
           <Routes>
              <Route path="/" element={
-               <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 h-full">
-                 <div className="flex-1 flex flex-col gap-6 lg:gap-8">
+               <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 flex-1 w-full">
+                 <div className="flex-none lg:flex-1 w-full flex flex-col gap-6 lg:gap-8 h-[380px] sm:h-[400px] lg:h-auto">
                    <FluidSlider onRecord={handleRecord} />
                  </div>
-                 <div className="flex-[1.2] flex flex-col gap-6 lg:gap-8 min-h-[400px] lg:min-h-0">
+                 <div className="flex-none lg:flex-[1.2] w-full flex flex-col gap-6 lg:gap-8 h-[400px] lg:h-auto lg:min-h-0">
                    <WaterTank records={records} alertThreshold={3000} isRefreshing={isRefreshing} />
                  </div>
                </div>
              } />
              <Route path="/dashboard" element={
-               <div className="flex flex-col lg:grid lg:grid-cols-2 gap-6 lg:gap-8 w-full h-full">
+               <div className="flex flex-col lg:grid lg:grid-cols-2 gap-6 lg:gap-8 flex-1 w-full">
                  <div className="flex flex-col gap-6 lg:gap-8">
                    <BalanceRings records={records} />
                    <ActivityGrid records={records} />
                  </div>
-                 <div className="flex flex-col gap-6 lg:gap-8">
+                 <div className="flex flex-col gap-6 lg:gap-8 min-h-[300px] lg:min-h-0">
                    <LedgerChart records={records} />
                  </div>
                </div>
              } />
              <Route path="/history" element={
-               <div className="w-full h-full flex flex-col">
+               <div className="flex-1 w-full flex flex-col">
                  <RecordList records={records} onUpdate={handleUpdate} />
                </div>
              } />
